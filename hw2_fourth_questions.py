4ee518213d9f337a11c0fe561b7f912a91085cba
ElvisCasco <ecasco1@gmail.com>
2025-10-01 09:47:42 -0600

    Modify hw2_fourth_questions.py

--- hw2_fourth_questions.py
+++ hw2_fourth_questions.py
@@ -1,1 +1,12 @@
-hw2_fourth_questions.py
+###############
+# Use the data in covid.csv for this exercise
+#
+# 10) In a separate file, write a piece of code that
+# loads the covid.csv file and prints the list of countries
+#  and the total average of death/confirmed among those countries
+# for those countries that have more than 500, 1000 and 5000
+# active cases respectively.
+# Follow DRY principles in order to complete this exercise.
+#
+#
+# #
