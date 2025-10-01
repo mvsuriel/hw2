9a0c27430fb680e77a6514ce0f6b100b2af440fd
ElvisCasco <ecasco1@gmail.com>
2025-10-01 09:44:57 -0600

    Modify hw2_third_questions.py

---
+++ hw2_third_questions.py
@@ -0,0 +1,28 @@
+# 7)
+# Create a function called "total_registered_cases"
+# that has 2 parameters:
+# 1) The data structure described above.
+# 2) A string with the country name.
+#
+# The function should return the total number of cases
+# registered so far in that country
+
+
+# 8)
+# Create a function called "total_registered_cases_per_country"
+# that has 1 parameter:
+# 1) The data structure described above.
+#
+# The function should return a dictionary with a key
+# per each country and as value the total number of cases
+# registered so far that the country had
+#
+
+
+# 9)
+# Create a function called "country_with_most_cases"
+# that has 1 parameter:
+# 1) The data structure described above
+#
+# The function should return the country with the
+# greatest total amount of cases
