5090ff35033e8bc88cf33b43ac3a0564a865eca7
ElvisCasco <ecasco1@gmail.com>
2025-10-05 15:11:56 -0600

    Modify hw2_fourth_questions.py

--- hw2_fourth_questions.py
+++ hw2_fourth_questions.py
@@ -9,8 +9,6 @@
 # Follow DRY principles in order to complete this exercise.
 #
 #
-import pandas as pd
-
 filename = 'https://raw.githubusercontent.com/mvsuriel/hw2/refs/heads/main/covid.csv'
 
 def analyze_covid(filename):
@@ -22,6 +20,7 @@ def analyze_covid(filename):
   (more than 500, 1000 and 5000 active cases)
   for covid cases using the filename specified as input.
   '''
+  import pandas as pd
   df = pd.read_csv(filename)
 
   thresholds = [500, 1000, 5000]
@@ -53,3 +52,49 @@ def analyze_covid(filename):
 
 # Analyze the covid_df DataFrame and display the results
 print(analyze_covid(filename))
+
+
+#### Alternate function, selecting a specific threshold (higher than t active cases): ###
+def analyze_covid_t(filename,t):
+  '''
+  Returns the cases of covid by country using a threshold (t). 
+  Also returns the number of countries and 
+  the average death/confirmed for countries with
+  active cases higher than the threshold
+  for covid cases using the filename specified as input.
+  '''
+  import pandas as pd
+  df = pd.read_csv(filename)
+  df['death_confirmed'] = df['Deaths'] / df['Confirmed']
+  filtered_df = df[df['Active'] > t]
+  print(f'\nMean of death_confirmed for countries with more than {t} active cases: {filtered_df["death_confirmed"].mean()}')
+  print(f'\nNumber of countries with more than {t} active cases: {filtered_df['Country'].count()}\n')
+  return filtered_df
+
+t = 1000
+print(analyze_covid_t(filename,t))
+
+### EXTRA: Analyze cases by intervals ###
+def analyze_covid_interval(filename):
+  '''
+  Returns the cases of covid by country using intervals. 
+  Also returns a dataframe with number of countries and 
+  the average death/confirmed by interval)
+  for covid cases using the filename specified as input.
+  '''
+  import pandas as pd
+  df = pd.read_csv(filename)
+  bins = [0, 500,1000,5000, df['Active'].max() + 1] # Ensure the last bin edge is greater than the max age
+  labels = ['<=500','>500', '>1000', '>5000']
+  df['interval'] = pd.cut(df['Active'], bins=bins, labels=labels, right=False, duplicates='drop')
+  
+  df['death_confirmed'] = df['Deaths'] / df['Confirmed']
+  df = df.sort_values('Active')
+  average_death_confirmed_and_count = df.groupby('interval', observed=False).agg(
+      average_death_confirmed=('death_confirmed', 'mean'),
+      number_of_countries=('Country', 'count')
+  ).reset_index()
+  return average_death_confirmed_and_count
+
+print('\n')
+print(analyze_covid_interval(filename))
