f4434cab952fcc125579a78b5d62eb593fa32104
ElvisCasco <ecasco1@gmail.com>
2025-10-05 14:42:35 -0600

    Modify hw2_fourth_questions.py

--- hw2_fourth_questions.py
+++ hw2_fourth_questions.py
@@ -13,35 +13,43 @@ import pandas as pd
 
 filename = 'https://raw.githubusercontent.com/mvsuriel/hw2/refs/heads/main/covid.csv'
 
-def analyze_covid(df):
-    df = pd.read_csv(filename)
+def analyze_covid(filename):
+  '''
+  Returns the countries that have more than 500, more than 1000 and 
+  more than 5000 active cases in a list. 
+  Also returns a dataframe with number of countries and 
+  the average death/confirmedthe results by threshold 
+  (more than 500, 1000 and 5000 active cases)
+  for covid cases using the filename specified as input.
+  '''
+  df = pd.read_csv(filename)
 
-    thresholds = [500, 1000, 5000]
-    results = []
+  thresholds = [500, 1000, 5000]
+  results = []
 
-    for threshold in thresholds:
-        filtered_df = df[df['Active'] > threshold]
-        average_death_confirmed = (filtered_df['Deaths'] / filtered_df['Confirmed']).mean()
-        countries = ', '.join(filtered_df['Country'].tolist())
-        num_countries = len(filtered_df)
-        results.append({
-            'Threshold': threshold, 
-            'Average Death/Confirmed': average_death_confirmed, 
-            'Number of Countries': num_countries,
-            'Countries': countries, 
-            })
+  for threshold in thresholds:
+      filtered_df = df[df['Active'] > threshold]
+      average_death_confirmed = (filtered_df['Deaths'] / filtered_df['Confirmed']).mean()
+      countries = ', '.join(filtered_df['Country'].tolist())
+      num_countries = len(filtered_df)
+      results.append({
+          'Threshold': threshold, 
+          'Number of Countries': num_countries,
+          'Average Death/Confirmed': average_death_confirmed, 
+          'Countries': countries, 
+          })
 
-    results_df = pd.DataFrame(results)
+  results_df = pd.DataFrame(results)
 
-    # Print the countries for each threshold inside the function
-    def countries_thresh(value):
-        res = results_df[results_df['Threshold'] == value]['Countries'].iloc[0]
-        return res
-    print(f'\nMore than 500 active cases: {countries_thresh(500)}')
-    print(f'\nMore than 1000 active cases: {countries_thresh(1000)}')
-    print(f'\nMore than 5000 active cases: {countries_thresh(5000)}\n')
-    
-    return results_df
+  # Print the countries for each threshold inside the function
+  def countries_thresh(value):
+      res = results_df[results_df['Threshold'] == value]['Countries'].iloc[0]
+      return res
+  print(f'\nMore than 500 active cases: {countries_thresh(500)}')
+  print(f'\nMore than 1000 active cases: {countries_thresh(1000)}')
+  print(f'\nMore than 5000 active cases: {countries_thresh(5000)}\n')
+  
+  return results_df
 
 # Analyze the covid_df DataFrame and display the results
-analyze_covid(filename)
+print(analyze_covid(filename))
